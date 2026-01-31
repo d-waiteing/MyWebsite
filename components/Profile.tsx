e0d6cb220f5563a5f7430768566aad764ade3b69
David Waite <daveycrockett02@gmail.com>
2026-01-31 13:53:21 -0600

    Begin update

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -15,11 +15,11 @@ export default function Profile() {
         <div className="flex flex-grow md:justify-end md:w-fit pb-5">
           <Icon imageHref="github.svg" link="https://github.com/d-waite/" />
           <Icon imageHref="linkedin.svg" link="https://www.linkedin.com/in/dgwcsm/" />
-          <Icon imageHref="file-text.svg" link="DGW_Resume Summer 2023.pdf" />
         </div>
       </div>
-      <p className="text-lg">
-        I am a Summa Cum Laude graduate from Colorado School of Mines, and I have a passion for creating high-quality, extendable software. I also have experience in data science and machine learning, including several different types of neural networks. Check out some of my projects!
+      <h1 className="text-4xl pb-3">About Me</h1>
+      <p className="text-lg p-2">
+        I'm a software engineer specializing in full-stack web development. A 2023 Summa Cum Laude graduate from Colorado School of Mines, I have professional experience building and improving software systems. At KE Andrews, I worked on in-house software where I developed new modules from the ground up, implemented key enhancements, delivered critical bug fixes and support, and managed project tickets throughout the development lifecycle. I combine technical expertise with strong project management skills to deliver quality solutions.
       </p>
     </div>
   );
