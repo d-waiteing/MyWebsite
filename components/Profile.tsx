c05c5bfbc9b71845e040b4b9b4c10849f46fafea
David Waite <daveycrockett02@gmail.com>
2026-02-15 19:34:59 -0600

    bleh

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -20,7 +20,7 @@ export default function Profile() {
       </div>
       <h1 className="text-4xl pb-3">About Me</h1>
       <p className="text-lg p-2">
-        I'm a software engineer specializing in full-stack web development. A 2023 Summa Cum Laude graduate from Colorado School of Mines, I have professional experience building and improving software systems. At KE Andrews, I worked on in-house software where I developed new systems from the ground up, implemented key enhancements, delivered critical bug fixes and support, and managed project tickets throughout the development lifecycle. I combine technical expertise with strong project management skills to deliver quality solutions.
+        I am a software engineer specializing in full-stack web development. A 2023 Summa Cum Laude graduate from Colorado School of Mines, I have professional experience building and improving software systems. At KE Andrews, I worked on in-house software where I developed new systems from the ground up, implemented key enhancements, delivered critical bug fixes and support, and managed project tickets throughout the development lifecycle. I combine technical expertise with strong project management skills to deliver quality solutions.
       </p>
     </div>
   );
